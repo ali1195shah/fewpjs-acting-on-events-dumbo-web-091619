54dbeed28089f7ee411f93b1fb1ee0970487bf87
Ali <ali1195shah@gmail.com>
2019-10-29 16:59:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+let dodger = document.getElementById('dodger');
+// console.log(dodger);
+  
+  document.addEventListener("keydown", function(event) {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    if (event.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+  });  
+
+  function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+
+  }
